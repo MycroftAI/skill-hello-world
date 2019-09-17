91bfe12d72988c393852c34aab4a4cec7a1a1126
Kris Gesling <krisgesling@gmail.com>
2019-09-17 16:41:31 +0930

    update init description

--- __init__.py
+++ __init__.py
@@ -20,31 +20,36 @@ from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 class HelloWorldSkill(MycroftSkill):
     def __init__(self):
+        """ The __init__ method is called when the Skill is first constructed.
+        It is often used to declare variables or perform setup actions, however
+        it cannot utilise MycroftSkill methods as the class does not yet exist.
+        """
         MycroftSkill.__init__(self)
+        self.learning = True
 
     def initialize(self):
-        # Perform any final setup needed for the skill here.
-        # This function is invoked after the skill is fully constructed and
-        # registered with the system. Intents will be registered and Skill
-        # settings will be available.
+        """ Perform any final setup needed for the skill here.
+        This function is invoked after the skill is fully constructed and
+        registered with the system. Intents will be registered and Skill
+        settings will be available."""
         my_setting = self.settings.get('my_setting')
 
     @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
     def handle_thank_you_intent(self, message):
-        # This is an Adapt intent handler, it is triggered by a keyword.
+        """ This is an Adapt intent handler, it is triggered by a keyword."""
         self.speak_dialog("welcome")
 
     @intent_file_handler('HowAreYou.intent')
     def handle_how_are_you_intent(self, message):
-        # This is a Padatious intent handler.
-        # It is triggered using a list of sample phrases.
+        """ This is a Padatious intent handler.
+        It is triggered using a list of sample phrases."""
         self.speak_dialog("how.are.you")
 
     @intent_handler(IntentBuilder('HelloWorldIntent')
                     .require('HelloWorldKeyword'))
     def handle_hello_world_intent(self, message):
-        # Skills can log useful information. These will appear in the CLI and
-        # the skills.log file.
+        """ Skills can log useful information. These will appear in the CLI and
+        the skills.log file."""
         self.log.info("There are five types of log messages: "
                       "info, debug, warning, error, and exception.")
         self.speak_dialog("hello.world")
